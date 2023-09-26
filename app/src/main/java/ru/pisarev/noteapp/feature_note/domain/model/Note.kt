5e670cb28206988cc8012ef7c71bc77e701d4b74
pisarev <ivpisarev@yandex.ru>
2023-09-26 12:18:01 +0700

    Setting up the SQL Database and NoteRepository

---
+++ app/src/main/java/ru/pisarev/noteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,22 @@
+package ru.pisarev.noteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ru.pisarev.noteapp.ui.theme.BabyBlue
+import ru.pisarev.noteapp.ui.theme.LightGreen
+import ru.pisarev.noteapp.ui.theme.RedOrange
+import ru.pisarev.noteapp.ui.theme.RedPink
+import ru.pisarev.noteapp.ui.theme.Violet
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, RedPink, BabyBlue, Violet, LightGreen)
+    }
+}
